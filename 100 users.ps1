5fbaa32ad1d09484988f0aa51178758267d6e070
ormashlay <164863096+ormashlay@users.noreply.github.com>
2024-04-10 14:36:26 +0300

    Add files via upload

---
+++ 100 users.ps1
@@ -0,0 +1,17 @@
+﻿
+Install-Module -Name AzureAD
+
+
+Connect-AzureAD
+
+
+for ($i = 1; $i -le 100; $i++) {
+    $displayName = "User$i"
+    $userPrincipalName = "user$i@ordomain.onmicrosoft.com" 
+    $passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $passwordProfile.Password = "Qwerty654321!"  
+
+    $newUser = New-AzureADUser -DisplayName $displayName -AccountEnabled $true -UserPrincipalName $userPrincipalName -PasswordProfile $passwordProfile
+    
+    Write-Output "User $displayName created."
+}
