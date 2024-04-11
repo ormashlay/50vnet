68aeb3a68bbb734c5e1ca2cb85f3864e5497273c
ormashlay <164863096+ormashlay@users.noreply.github.com>
2024-04-11 10:30:01 +0300

    container Creation

---
+++ container Creation.ps1
@@ -0,0 +1,16 @@
+﻿
+Connect-AzAccount
+
+$storageAccountName = "telavivstorage"
+$resourceGroupName = "tel-aviv"
+
+
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -ResourceGroupName $resourceGroupName
+
+
+$containerNames = @("container1", "container2", "container3")
+
+foreach ($tel1container in $tel2container ) {
+    New-AzStorageContainer -Name $containerName -Context $ctx
+}
+
