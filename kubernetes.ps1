d82326079777dc369d8287a9071074d9330324e5
ormashlay <164863096+ormashlay@users.noreply.github.com>
2024-04-11 11:19:39 +0300

    Creation

---
+++ kubernetes.ps1
@@ -0,0 +1,17 @@
+﻿
+Install-Module -Name Kubernetes
+
+choco install minikube
+
+
+minikube start
+
+
+mkdir my-server
+
+
+New-Item -Path my-server -ItemType File -Name server.yaml
+
+
+code my-server\server.yaml
+
