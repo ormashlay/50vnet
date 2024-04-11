478229bf90396fb95d6836c13246b5f29a10d7e7
ormashlay <164863096+ormashlay@users.noreply.github.com>
2024-04-11 11:25:16 +0300

    YAML standard

---
+++ YAML.ps1
@@ -0,0 +1,10 @@
+﻿apiVersion: v1
+kind: Pod
+metadata:
+  name: my-server-pod
+spec:
+  containers:
+  - name: my-server-container
+    image: my-server-image:latest
+    ports:
+    - containerPort: 8080
